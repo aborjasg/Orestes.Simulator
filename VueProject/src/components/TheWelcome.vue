6faeadf1f1b08874328335a00bced51179167434
Alex BG <aborjasg@protonmail.com>
2025-08-05 22:55:14 -0700

    Add new project (Vue)

---
+++ VueProject/src/components/TheWelcome.vue
@@ -0,0 +1,28 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Customers</template>
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Weather Forecast</template>
+  </WelcomeItem>
+
+</template>
