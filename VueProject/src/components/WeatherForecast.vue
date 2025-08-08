295dd18af5722ed28886d3365d310dd8b869eabb
Alex BG <aborjasg@protonmail.com>
2025-08-07 21:39:24 -0600

    Update call API in Vue

--- VueProject/src/components/WeatherForecast.vue
+++ VueProject/src/components/WeatherForecast.vue
@@ -1,30 +1,33 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+interface WeatherForecast {
+  date: String;
+  temperatureC: Number;
+  summary: String;
+}
+
+let list: WeatherForecast[] = [];
+let data: string = "";
+
+onMounted( async () => {
+    try {
+        console.log(`data: ${ data }`);
+        const apiURL = 'http://localhost:5062/api/WeatherForecast';
+        const response = await fetch(apiURL);
+        console.log(`apiURL: ${apiURL}`);
+        list = await response.json() as WeatherForecast[];    
+        data = JSON.stringify(list, null, 2);
+        console.log(`data: ${ data } - list: ${list.length}`);
+    } catch (error) {
+        console.error(error);
+    }
+});
+</script>
+
 <template>
-  <div onload="mounted();">
-    <button @click="mounted">Invoke</button>
-    <!-- <p>{{ data }}</p> -->
+  <div>
+    <p v-if="list.length>0">{{ list[0].date }}</p>
+    <p v-else>Loading data...</p>
   </div>
 </template>
-
-<script>
-export default {    
-    methods: { 
-        data() {
-            return {
-                data: []
-            };
-        },
-        async mounted() {
-            try {
-                const apiURL = 'http://localhost:5062/api/WeatherForecast';
-                const response = await fetch(apiURL);
-                console.log(`apiURL: ${apiURL}`);
-                let json = await response.json();
-                this.data = JSON.stringify(json, null, 2);
-                console.log(`data: ${ this.data }`);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    }
-};
-</script>
