34fb1bb825fba42f6e09cbdd21a4ee78bbb07697
Alex BG <aborjasg@protonmail.com>
2025-09-01 22:46:06 -0600

    Add GraphQL service [#8]

---
+++ WebAPI/Orestes.Simulator.WebAPI/Controllers/WeatherForecastSchema.cs
@@ -0,0 +1,13 @@
+﻿using Orestes.Simulator.DataSource.Models;
+using GraphQL.Types;
+
+namespace Orestes.Simulator.WebAPI.Controllers
+{
+    public class WeatherForecastSchema : Schema
+    {
+        public WeatherForecastSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = provider.GetRequiredService<WeatherForecastQuery>();
+        }
+    }
+}
